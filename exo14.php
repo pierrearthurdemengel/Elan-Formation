6bf27c4b59f645907e23ce1e31059f556af45bc2
Pierre-Arthur DEMENGEL <pademengel@gmail.com>
2023-02-07 12:04:34 +0100

    simpsons

--- exo14.php
+++ exo14.php
@@ -1,3 +1,25 @@
 <?php
 //exercice 14
-define($DATE,  )
+$time = 
+function ago($time) {
+    $time = strtotime($time);
+    $diff_time = time() - $time;
+
+        if($diff_time<1) {return 'à l\'instant';}
+}
+
+$sec = array(   31556926 => 'an',
+                2629743.83 => 'mois',
+                86400 => 'jour',
+                3600 => 'minute',
+                1 => 'seconde'        
+);
+
+foreach($sec as $sec => $value) {
+    $div = $diff_time / $sec;
+    if($div >= 1) {
+        $time_ago = round($div);
+        $time_type = $value;
+        return 'Age de la personne: ';
+    };
+};
