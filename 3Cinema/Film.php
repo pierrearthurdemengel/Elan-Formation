d13bd273844b4245f92430ce96d548b914302bb2
pierrearthurdemengel <pademengel@gmail.com>
2023-02-17 15:23:44 +0100

    Correction Index, Genre, Film et real v2

--- 3Cinema/Film.php
+++ 3Cinema/Film.php
@@ -7,7 +7,7 @@ class Film
         private DateTime $date_sortie;
         private int $duree;
         private string $synopsis;
-        private string $genre;
+        private Genre $genre;
         private Realisateur $realisateur;
         private $roles = array();
     
@@ -49,7 +49,7 @@ class Film
 {
     $FilmInfo = "<h1>Film : ". $this->film."</h1>";
     $FilmInfo .= " ";
-    foreach ($this->film as $film) {
+    foreach ($film[] as $film) {
         $FilmInfo .= "$film";
     }
     $FilmInfo .= " ";
@@ -57,6 +57,6 @@ class Film
 }
 }
 
-FilmSkyfall = new Film("Skyfall", 2005-06-15, '140', "Laissé pour mort après une mission en Turquie qui a tourné au désastre, l'agent britannique James Bond, nom de code 007, réapparaît à Londres lorsqu'il apprend par les informations d'un journal télévisé qu'un attentat a été commis contre le M16. Cet événement ébranle considérablement l'autorité de la directrice M.", "Action", "Sam Mendes");
-FilmBatman = new Film("Batman Begins", 2012-09-26, 143, "Comment un homme seul peut-il changer le monde ? Telle est la question qui hante Bruce Wayne depuis cette nuit tragique où ses parents furent abattus sous ses yeux, dans une ruelle de Gotham City. Torturé par un profond sentiment de colère et de culpabilité, le jeune héritier de cette richissime famille fuit Gotham pour un long et discret voyage à travers le monde. Le but de ses pérégrinations : sublimer sa soif de vengeance en trouvant de nouveaux moyens de lutter contre l'injustice.", "Action", "Christopher Nolan");
+$FilmSkyfall = new Film("Skyfall", 2005-06-15, '140', "Laissé pour mort après une mission en Turquie qui a tourné au désastre, l'agent britannique James Bond, nom de code 007, réapparaît à Londres lorsqu'il apprend par les informations d'un journal télévisé qu'un attentat a été commis contre le M16. Cet événement ébranle considérablement l'autorité de la directrice M.", "Action", "Sam Mendes");
+$FilmBatman = new Film("Batman Begins", 2012-09-26, 143, "Comment un homme seul peut-il changer le monde ? Telle est la question qui hante Bruce Wayne depuis cette nuit tragique où ses parents furent abattus sous ses yeux, dans une ruelle de Gotham City. Torturé par un profond sentiment de colère et de culpabilité, le jeune héritier de cette richissime famille fuit Gotham pour un long et discret voyage à travers le monde. Le but de ses pérégrinations : sublimer sa soif de vengeance en trouvant de nouveaux moyens de lutter contre l'injustice.", "Action", "Christopher Nolan");
 ?>
--- 3Cinema/Genre.php
+++ 3Cinema/Genre.php
@@ -1,30 +1,20 @@
 <?php 
 class Genre
     {
-        private array $films;
         private string $genre;
 
 
         
         public function __construct(string $genre)
         { //initialisation
-            $this->films = [];
             $this->genre = $genre;
         }
         
         // GETTERS
-    public function getFilm() : array
-            {
-                return $this->films;
-            }
             
     public function __toString()
             {
                 return $this->genre;
             }
-    public function addFilm(Film $film) 
-    {
-        $this->films[] = $film;
-    }
-}
+        }
 ?>
