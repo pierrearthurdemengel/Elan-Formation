ba482d1d2036bd483122394d021c8912a20df1c4
pierrearthurdemengel <pademengel@gmail.com>
2023-03-01 10:13:50 +0100

    Commit ligne 19

--- appli/traitement.php
+++ appli/traitement.php
@@ -5,11 +5,25 @@ formulaire), ajoutera le produit avec son nom, son prix, sa quantité et le tota
     session_start();
     if(isset($_POST['submit'])){
 
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION); // filter = protection injection SQL
+        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+
+        if($name && $price && $qtt){
+            $product = [
+                "name" => $name,
+                "price" => $price,
+                "quantity" => $qtt,
+                "total" => $price*$qtt
+
+            ];
+
+            $_SESSION['product'][] = $product;
+
     }
+}
     header("location:index.php"); // header en dernier dans le scipt ou utliser tout de suite après exit() ou die()
     //Impossible de mettre deux headers , le second écraseant le premier
     
-
-
 
 ?>
