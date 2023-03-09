ffec78999fe0b53df05a893cc4194c4c9978c42c
pierrearthurdemengel <pademengel@gmail.com>
2023-03-09 15:52:21 +0100

    Gaulois 5

--- SQL_Gaulois/requetes.sql
+++ SQL_Gaulois/requetes.sql
@@ -29,7 +29,10 @@ ORDER BY nbperso DESC
 -- 5. Nom, date et lieu des batailles, classées de la plus récente à la plus ancienne (dates affichées 
 -- au format jj/mm/aaaa).
 
-
+SELECT nom_bataille, DATE_FORMAT(date_bataille, "%d-%m-%Y") AS dateB, nom_lieu
+FROM bataille b
+INNER JOIN lieu l ON l.id_lieu = b.id_lieu
+ORDER BY date_bataille
 
 -- 6. Nom des potions + coût de réalisation de la potion (trié par coût décroissant).
 
@@ -37,19 +40,19 @@ ORDER BY nbperso DESC
 
 -- 8. Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village 
 -- gaulois'.
-
+0
 -- 9. Nom des personnages et leur quantité de potion bue (en les classant du plus grand buveur 
 -- au plus petit).
 
 -- 10. Nom de la bataille où le nombre de casques pris a été le plus important.
-
+0
 -- 11. Combien existe-t-il de casques de chaque type et quel est leur coût total ? (classés par 
 -- nombre décroissant)
 
 -- 12. Nom des potions dont un des ingrédients est le poisson frais.
 
 -- 13. Nom du / des lieu(x) possédant le plus d'habitants, en dehors du village gaulois.
-
+0
 -- 14. Nom des personnages qui n'ont jamais bu aucune potion.
 
 -- 15. Nom du / des personnages qui n'ont pas le droit de boire de la potion 'Magique'.
