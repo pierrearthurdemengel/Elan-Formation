35ff69a19bc101bc1236e4303f5b568d830c83f5
pierrearthurdemengel <pademengel@gmail.com>
2023-03-15 10:14:17 +0100

    b.

--- CinemaFormulaire/Film_sql.sql
+++ CinemaFormulaire/Film_sql.sql
@@ -7,3 +7,22 @@ INNER JOIN personne p ON r.personne_id = p.id_personne
 
 -- b. Liste des films dont la durée excède 2h15 classés par durée (du plus long au plus court)
 
+SELECT 
+    f.titre,          
+    f.annee,         
+    TIME_FORMAT(SEC_TO_TIME(f.duree*60), '%H:%i') as temps_format,  
+    f.synopsis,      
+    f.note5,        
+    f.lien_affiche,   
+    p.nom,             
+    p.prenom,         
+    p.sexe,            
+    p.date_naissance   
+FROM 
+    film f              
+INNER JOIN 
+    realisateur r ON f.realisateur_id = r.id_realisateur 
+INNER JOIN 
+    personne p ON r.personne_id = p.id_personne          
+WHERE 
+    SEC_TO_TIME(f.duree*60) > '02:15'  
